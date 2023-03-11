0f33281e5a85d2c17d389f95623aa2f0bbfd6717
Anshika bhatt <119714902+anshikabhatt@users.noreply.github.com>
2023-03-11 13:03:11 +0530

    Update data_timestamps.rb

--- data_timestamps.rb
+++ data_timestamps.rb
@@ -1,9 +1,7 @@
 require 'csv'
 
-# Load the sequence CSV file into memory
 sequence = CSV.read('sequence.csv', headers: true)
 
-# Sort the sequence by timestamp in descending order
 sequence.sort_by! { |row| row['timestamp'] }.reverse!
 
 # Create an empty hash to store the most recent temperature for each city
@@ -14,16 +12,13 @@ sequence.each do |row|
   city = row['city']
   temperature = row['temperature']
 
-  # If we haven't seen this city before, add it to the hash with the current temperature
   if !latest_temperatures[city]
     latest_temperatures[city] = temperature
 
-  # If we have seen this city before, update the temperature if the current timestamp is more recent
   elsif row['timestamp'] > latest_temperatures[city + '_timestamp']
     latest_temperatures[city] = temperature
   end
 
-  # Store the timestamp for this city
   latest_temperatures[city + '_timestamp'] = row['timestamp']
 end
 
