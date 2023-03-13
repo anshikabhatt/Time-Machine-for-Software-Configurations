cd834e9eaa1fdc38fa7664a34fc0a9c47b69cfc9
Anshika bhatt <119714902+anshikabhatt@users.noreply.github.com>
2023-03-13 12:19:42 +0530

    Update timemachine (1).rb

--- timemachine (1).rb
+++ timemachine (1).rb
@@ -1,8 +1,14 @@
-class TimeMachine
+require 'json'
+
+class Configurations
   attr_reader :configurations
 
   def initialize(directory)
     @configurations = parse_directory(directory)
+  end
+
+  def get_configuration(name)
+    @configurations[name]
   end
 
   private
@@ -20,5 +26,44 @@ class TimeMachine
 
   def parse_file(file)
     JSON.parse(File.read(file))
+  end
+end
+
+class TimeMachine
+  attr_reader :configurations
+
+  def initialize(directory)
+    @configurations = Configurations.new(directory)
+  end
+
+  def snapshots
+    snapshot_files.map do |file|
+      timestamp = parse_timestamp(file)
+      data = parse_data(file)
+      Snapshot.new(timestamp, data)
+    end
+  end
+
+  private
+
+  def snapshot_files
+    Dir.glob(File.join(configurations.configurations['snapshots_directory'], '*.json')).sort
+  end
+
+  def parse_timestamp(file)
+    File.basename(file, '.json').to_i
+  end
+
+  def parse_data(file)
+    JSON.parse(File.read(file))
+  end
+end
+
+class Snapshot
+  attr_reader :timestamp, :data
+
+  def initialize(timestamp, data)
+    @timestamp = timestamp
+    @data = data
   end
 end
